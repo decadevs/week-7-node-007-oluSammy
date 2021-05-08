693cd9c80e9f9b6f2f4241bc4a51c1fca2a61526
oluSammy <olumorinsammy@gmail.com>
2021-05-08 23:07:13 +0100

    heroku commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node task-two/bin/www
--- task-two/src/app.ts
+++ task-two/src/app.ts
@@ -5,7 +5,6 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
 import calculateRouter from './routes';
-import fetchRecords from './routes/fetchRecords';
 
 const app = express();
 
